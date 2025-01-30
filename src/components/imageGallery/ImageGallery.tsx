7fa9f2cf35c307620c1e7d5010e0ec8be730c153
espiritukarl <karlespiritukarl@gmail.com>
2025-01-30 08:26:27 +0800

    Fixed how the images are displayed in carousel

--- src/components/imageGallery/ImageGallery.tsx
+++ src/components/imageGallery/ImageGallery.tsx
@@ -5,13 +5,16 @@ import "./ImageGallery.css";
 export function ImageGallery({ gallery }: ImageGalleryContainer) {
   return (
     <div className="image-gallery">
-      {gallery.map((image, idx) => (
-        <img
-          key={`image-gallery-${idx}`}
-          src={image}
-          alt={`Gallery Image ${idx}`}
-        />
-      ))}
+      {gallery.map(
+        (image: string, idx: number) =>
+          idx < 4 && (
+            <img
+              key={`image-gallery-${idx}`}
+              src={image}
+              alt={`Gallery Image ${idx}`}
+            />
+          )
+      )}
     </div>
   );
 }
