df966c551a74fc15e512f87e76d13493be6958ae
espiritukarl <karlespiritukarl@gmail.com>
2025-02-03 13:54:13 +0800

    Added AddToCart in conjunction with CartContext, to update Cart

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,6 +1,10 @@
+//utils
 import { ProductData } from "../../common/types";
+import { useCart } from "../../utils/CartContext";
 
 export function ProductCard({ product }: { product: ProductData }) {
+  const { addToCart } = useCart();
+
   return (
     <article className="product-card-container">
       <h4 className="product-tile">{product.title}</h4>
@@ -13,6 +17,9 @@ export function ProductCard({ product }: { product: ProductData }) {
         <div className="product-rating">{product.rating.rate}</div>
         <div className="product-amount-bought">{product.rating.count}</div>
       </div>
+      <button className="add-to-cart" onClick={() => addToCart(product)}>
+        Add to Cart
+      </button>
     </article>
   );
 }
