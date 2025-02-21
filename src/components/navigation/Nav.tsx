42100f072c7de79ce9080c08a4d2443395a0c93c
espiritukarl <karlespiritukarl@gmail.com>
2025-02-21 09:35:45 +0800

    Added mui badge for cart icon

--- src/components/navigation/Nav.tsx
+++ src/components/navigation/Nav.tsx
@@ -6,23 +6,44 @@ import { Link } from "react-router-dom";
 import { Cart } from "../cart/Cart";
 
 //utils
+import { CartProduct } from "../../common/types";
 import { useCart } from "../../utils/CartContext";
+import "./Nav.css";
+
+//MUI
+import Badge from "@mui/material/Badge";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+const CartBadge = ({ cartItems }: { cartItems: CartProduct[] }) => {
+  return (
+    <IconButton color="inherit">
+      <Badge
+        badgeContent={cartItems.reduce((acc, item) => acc + item.quantity, 0)}
+        color="primary"
+        showZero
+      >
+        <ShoppingCartIcon />
+      </Badge>
+    </IconButton>
+  );
+};
 
 export function Nav() {
   const [showCart, setShowCart] = useState<boolean>(false);
   const { cartItems } = useCart();
   return (
-    <nav>
+    <nav className="navigation-container">
       <ul>
-        <li>
+        <li className="navigation-links">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="navigation-links">
           <Link to="/shop">Shop</Link>
         </li>
       </ul>
       <div className="cart" onClick={() => setShowCart(!showCart)}>
-        CART ({cartItems.length})
+        <CartBadge cartItems={cartItems} />
       </div>
       {showCart && <Cart />}
     </nav>
