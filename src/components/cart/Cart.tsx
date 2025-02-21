c8a1409b67c43c6d9d7a8103d278d3c5cac211b2
espiritukarl <karlespiritukarl@gmail.com>
2025-02-21 16:13:34 +0800

    Final draft for Cart

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,24 +1,63 @@
 //utils
 import { useCart } from "../../utils/CartContext";
 import { CartProduct } from "../../common/types";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Divider from "@mui/material/Divider";
+import "./Cart.css";
 
-export function Cart() {
-  const { cartItems } = useCart();
+export function Cart({ onClose }: { onClose: () => void }) {
+  const { cartItems, clearCart } = useCart();
+
+  function CartItemsList() {
+    if (cartItems.length === 0) {
+      return <li className="product none">No products in Cart.</li>;
+    }
+    return cartItems.map((product: CartProduct) => (
+      <>
+        <li key={`${product.item.id}-cart`}>
+          <div className="product title">{product.item.title}</div>
+          <div className="product quantity">(Quantity: {product.quantity})</div>
+          <div className="product price">
+            ${product.item.price * product.quantity}
+          </div>
+        </li>
+      </>
+    ));
+  }
   return (
     <aside className="cart-container">
-      <h2>My Cart:</h2>
-      <ul>
-        {cartItems.map((product: CartProduct) => (
-          <li key={`${product.item.id}-cart`}>
-            {product.item.title} ({product.quantity})
-          </li>
-        ))}
-      </ul>
+      <div className="cart-items">
+        <h2>
+          <ShoppingCartIcon /> My Cart:
+        </h2>
+        <Divider sx={{ my: 3 }} />
+        <ul>
+          <CartItemsList />
+        </ul>
+      </div>
       <div className="cart-checkout">
-        <div className="cart-quantity">
-          Total Items: {cartItems.reduce((acc, item) => acc + item.quantity, 0)}
+        <div className="cart-price">
+          <Divider sx={{ my: 3 }} />
+          <div className="label">Total Price: </div>
+          <div className="total">
+            $
+            {cartItems.reduce(
+              (acc, item) => acc + item.item.price * item.quantity,
+              0
+            )}
+          </div>
         </div>
-        <button className="checkout">Checkout</button>
+        <button
+          className="checkout"
+          disabled={cartItems.length === 0}
+          onClick={() => {
+            onClose();
+            clearCart();
+          }}
+        >
+          Checkout ({cartItems.reduce((acc, item) => acc + item.quantity, 0)}{" "}
+          items)
+        </button>
       </div>
     </aside>
   );
