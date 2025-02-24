c5a3655bbd4da10074446a5cefc80ea4b7afac0a
espiritukarl <karlespiritukarl@gmail.com>
2025-02-24 15:31:42 +0800

    Added Tooltip from mui to be able to hover over star rating. Removed the rating from being seen.

--- src/components/productCard/ProductCard.tsx
+++ src/components/productCard/ProductCard.tsx
@@ -1,6 +1,7 @@
 //react
 import { useState } from "react";
 import { Rating } from "react-simple-star-rating";
+import Tooltip from "@mui/material/Tooltip";
 
 //components
 import { ProductQuantity } from "../productQuantity/ProductQuantity";
@@ -8,6 +9,7 @@ import { ProductQuantity } from "../productQuantity/ProductQuantity";
 //utils
 import { ProductData } from "../../common/types";
 import { useCart } from "../../utils/CartContext";
+import "./ProductCard.css";
 
 export function ProductCard({ product }: { product: ProductData }) {
   const { addToCart } = useCart();
@@ -16,22 +18,43 @@ export function ProductCard({ product }: { product: ProductData }) {
 
   return (
     <article className="product-card-container">
-      <h4 className="product-tile">{product.title}</h4>
       <img
         src={product.image}
         alt={`${product.title} image`}
         className="product-img"
       />
+      <h4 className="product-tile">{product.title}</h4>
       <div className="product-price">${product.price}</div>
       <div className="product-buys-container">
-        <div className="product-rating">
-          <Rating
-            initialValue={roundedValue(product.rating.rate)}
-            allowFraction
-            readonly
-          />
-          {product.rating.rate}/5
-        </div>
+        <Tooltip
+          title={
+            <span
+              style={{
+                fontSize: "1rem",
+                padding: "4px",
+                display: "block",
+              }}
+            >
+              {product.rating.rate}/5
+            </span>
+          }
+          arrow
+          sx={{
+            "& .MuiTooltip-tooltip": {
+              fontSize: "1rem",
+            },
+          }}
+          placement="top-end"
+        >
+          <div className="product-rating">
+            <Rating
+              initialValue={roundedValue(product.rating.rate)}
+              showTooltip={false}
+              allowFraction
+              readonly
+            />
+          </div>
+        </Tooltip>
         <div className="product-amount-bought">{product.rating.count}</div>
       </div>
       <ProductQuantity
